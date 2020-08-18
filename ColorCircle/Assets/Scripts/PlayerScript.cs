32d2ab54970a5ff70c2dce7034e904aa881e9d9c
netef <netef10@gmail.com>
2020-08-18 04:38:13 +0300

    first logic

---
+++ ColorCircle/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private float jumpForce = 10f;
+    private bool mouseClicked = false;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseClicked = true;
+            Debug.Log("click");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (mouseClicked)
+        {
+            mouseClicked = false;
+            rigidbody.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
